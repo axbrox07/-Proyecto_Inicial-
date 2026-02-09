1a2442dc5484a034bbf18bfd4e82311b9a6ed2f3
Agustin <agustinbarrionuevo72@gmail.com>
2026-02-09 13:16:09 -0300

    agregue usuario y parlantes

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-console.log("milanesas")
+function Usuario(nombre){
+    console.log(`Bienvenido ${nombre}`)
+}
+Usuario()
+
+function muestra_parlantes(){
+    console.log("Bienvenido a nuestra tienda, estos son todos los articulos que tenemos disponibles para vos")
+}
+
+let Marcas_Parlantes = new Array()
+Marcas_Parlantes = ("JBL, Bose, Sony, KEF, Jamo, Somos, Dynaudio, Focal, RCF, Ultimate Ears")
+
+
 
