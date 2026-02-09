b5c2c82af8138e82899cc3a49a7238cd637a65e2
Agustin <agustinbarrionuevo72@gmail.com>
2026-02-09 16:34:54 -0300

    modificamos el index y agregamos pedir usuario

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-function Usuario(nombre){
-    console.log(`Bienvenido ${nombre}`)
+
+alert("BIENVENIDO AL SIMULADOR")
+
+const Marca_Parlantes = ["JBL, Sony, RCF, Dynaudio, KEF, Ultimate Ears, Genius, SVS, Genelec"]
+
+const productos = [
+    { nombre: "JBL", precio: 21000 },
+    { nombre: "Sony", precio: 18000 },
+    { nombre: "RCF", precio: 12000 },
+    { nombre: "Dynaudio", precio: 7000},
+    { nombre: "KEF", precio: 13000},
+    { nombre : "Ultimate Ears", precio: 15000},
+    { nombre : "Genius", precio: 16000},
+    { nombre : "SVS", precio: 11000},
+    { nombre : "Genelec", precio: 18000},
+];
+
+function Pedirusuario(){
+    let nombre = prompt("Bienvenido/a al simulador, por favor ingrese su nombre de usuario: ")
+    return nombre
 }
-Usuario()
+Pedirusuario()
 
 function muestra_parlantes(){
-    console.log("Bienvenido a nuestra tienda, estos son todos los articulos que tenemos disponibles para vos")
+    console.log("Bienvenido a nuestra tienda, estos son todos los productos que tenemos disponibles para vos")
+    console.log(Marca_Parlantes)
 }
-
-let Marcas_Parlantes = new Array()
-Marcas_Parlantes = ("JBL, Bose, Sony, KEF, Jamo, Somos, Dynaudio, Focal, RCF, Ultimate Ears")
-
-
+muestra_parlantes()
 
